965468bcbd031b3687725d41bd30fdb8fba29ace
mvegeta <944031211@qq.com>
2021-10-08 12:54:35 +0800

    Add files via upload

---
+++ utils/s2cnn/to_sphere.py
@@ -0,0 +1,128 @@
+import numpy as np
+import lie_learn.spaces.S2 as S2
+
+
+NORTHPOLE_EPSILON = 1e-3
+
+
+def get_projection_grid(b, grid_type="Driscoll-Healy"):
+    ''' returns the spherical grid in euclidean
+    coordinates, where the sphere's center is moved
+    to (0, 0, 1)'''
+    theta, phi = S2.meshgrid(b=b, grid_type=grid_type)
+    x_ = np.sin(theta) * np.cos(phi)
+    y_ = np.sin(theta) * np.sin(phi)
+    z_ = np.cos(theta)
+    return x_, y_, z_
+
+
+def project_2d_on_sphere(signal, grid, projection_origin=None):
+    ''' '''
+    if projection_origin is None:
+        projection_origin = (0, 0, 2 + NORTHPOLE_EPSILON)
+
+    rx, ry = project_sphere_on_xy_plane(grid, projection_origin)
+    sample = sample_bilinear(signal, rx, ry)
+
+    # ensure that only south hemisphere gets projected
+    sample *= (grid[2] <= 1).astype(np.float64)
+
+    # rescale signal to [0,1]
+    sample_min = sample.min(axis=(1, 2)).reshape(-1, 1, 1)
+    sample_max = sample.max(axis=(1, 2)).reshape(-1, 1, 1)
+
+    sample = (sample - sample_min) / (sample_max - sample_min)
+    sample *= 255
+    sample = sample.astype(np.uint8)
+
+    return sample
+
+
+def project_sphere_on_xy_plane(grid, projection_origin):
+    ''' returns xy coordinates on the plane
+    obtained from projecting each point of
+    the spherical grid along the ray from
+    the projection origin through the sphere '''
+
+    sx, sy, sz = projection_origin
+    x, y, z = grid
+    z = z.copy() + 1
+
+    t = -z / (z - sz)
+    qx = t * (x - sx) + x
+    qy = t * (y - sy) + y
+
+    xmin = 1/2 * (-1 - sx) + -1
+    ymin = 1/2 * (-1 - sy) + -1
+
+    # ensure that plane projection
+    # ends up on southern hemisphere
+    rx = (qx - xmin) / (2 * np.abs(xmin))
+    ry = (qy - ymin) / (2 * np.abs(ymin))
+
+    return rx, ry
+
+
+def sample_bilinear(signal, rx, ry):
+    ''' '''
+
+    signal_dim_x = signal.shape[1]
+    signal_dim_y = signal.shape[2]
+
+    rx *= signal_dim_x
+    ry *= signal_dim_y
+
+    # discretize sample position
+    ix = rx.astype(int)
+    iy = ry.astype(int)
+
+    # obtain four sample coordinates
+    ix0 = ix - 1
+    iy0 = iy - 1
+    ix1 = ix + 1
+    iy1 = iy + 1
+
+    bounds = (0, signal_dim_x, 0, signal_dim_y)
+
+    # sample signal at each four positions
+    signal_00 = sample_within_bounds(signal, ix0, iy0, bounds)
+    signal_10 = sample_within_bounds(signal, ix1, iy0, bounds)
+    signal_01 = sample_within_bounds(signal, ix0, iy1, bounds)
+    signal_11 = sample_within_bounds(signal, ix1, iy1, bounds)
+
+    # linear interpolation in x-direction
+    fx1 = (ix1-rx) * signal_00 + (rx-ix0) * signal_10
+    fx2 = (ix1-rx) * signal_01 + (rx-ix0) * signal_11
+
+    # linear interpolation in y-direction
+    return (iy1 - ry) * fx1 + (ry - iy0) * fx2
+
+
+def sample_within_bounds(signal, x, y, bounds):
+    ''' '''
+    xmin, xmax, ymin, ymax = bounds
+
+    idxs = (xmin <= x) & (x < xmax) & (ymin <= y) & (y < ymax)
+
+    if len(signal.shape) > 2:
+        sample = np.zeros((signal.shape[0], x.shape[0], x.shape[1]))
+        sample[:, idxs] = signal[:, x[idxs], y[idxs]]
+    else:
+        sample = np.zeros((x.shape[0], x.shape[1]))
+        sample[idxs] = signal[x[idxs], y[idxs]]
+    return sample
+
+
+def feature2sphere(x, in_channel, bandwidth):
+    signals = x.reshape(-1, in_channel, in_channel).astype(np.float64)
+    n_signals = signals.shape[0]
+    projections = np.ndarray(
+        (signals.shape[0], 2 * bandwidth, 2 * bandwidth),
+        dtype=np.uint8)
+    grid = get_projection_grid(b=bandwidth)
+
+    rotated_grid = grid
+
+    idxs = np.arange(0, min(n_signals,n_signals))
+    chunk = signals[idxs]
+    projections[idxs] = project_2d_on_sphere(chunk, rotated_grid)
